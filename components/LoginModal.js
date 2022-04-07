86c31368d8835d27ffa58f9ec440c224d1bded11
JaavLex <alexandre.javet@hotmail.com>
2022-04-07 13:01:19 +0200

    [feat] Improved login modal
    - Set all inputs to outlined variant
    - Added app logo to modal

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,55 +1,60 @@
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	TextField,
 } from '@mui/material';
 
 export function LoginModal({ open, onClose, onSuccess }) {
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <DialogTitle>Connect toi!</DialogTitle>
-      <form
-        // set no redirection to form
-        action="#"
-        method="POST"
-        onSubmit={(e) => {
-          e.preventDefault();
+	return (
+		<Dialog onClose={onClose} open={open}>
+			<DialogTitle>
+				<p className="txt-862">
+					Hang<span className="txt-8622">Now!</span>
+				</p>
+				<b>Inscris toi!</b>
+			</DialogTitle>
+			<form
+				// set no redirection to form
+				action="#"
+				method="POST"
+				onSubmit={e => {
+					e.preventDefault();
 
-          onSuccess();
+					onSuccess();
 
-          return false;
-        }}
-        name="email-login"
-        netlify
-      >
-        <DialogContent>
-          <DialogContentText>
-            Utilise ton email pour te connecter et pouvoir entrer en communication
-            avec la personne qui organise l'évènement.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={onClose}>
-            Fermer
-          </Button>
-          <Button type="submit" autoFocus onClick={onClose}>
-            Envoyer
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
-  );
+					return false;
+				}}
+				name="email-login"
+				netlify
+			>
+				<DialogContent>
+					<DialogContentText>
+						Utilise ton email pour te connecter et pouvoir entrer en communication
+						avec la personne qui organise l'évènement.
+					</DialogContentText>
+					<TextField
+						autoFocus
+						margin="dense"
+						id="name"
+						label="Adresse Mail"
+						type="email"
+						fullWidth
+						variant="outlined"
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={onClose} variant="outlined">
+						Fermer
+					</Button>
+					<Button type="submit" autoFocus onClick={onClose} variant="outlined">
+						Envoyer
+					</Button>
+				</DialogActions>
+			</form>
+		</Dialog>
+	);
 }

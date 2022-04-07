7b9d5bfa968abc067a42af73cbf442a7fea11393
villou <dkulic01@gmail.com>
2022-04-07 19:40:48 +0200

    redirect to homepage

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,11 @@
 import { TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 
 export default function Contact() {
   const [success, setSuccess] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     if (window.location.search.includes('success=true')) {
@@ -10,20 +13,19 @@ export default function Contact() {
     }
   }, []);
 
+  const onClickTitle = () => {
+    router.push('/');
+  };
+
   return (
     <div className="iphone-11-pro-x-1 flex-col-hstart-vstart clip-contents">
       <div className="header flex-row-vcenter-hsb">
-        <p className="txt-862">
+        <p className="txt-862" onClick={onClickTitle}>
           Hang<span className="txt-8622">Now!</span>
         </p>
-        <div className="frame-49220 flex-row-vcenter-hcenter">
-          <button className="button-join flex-row-vcenter-hcenter clip-contents">
-            <span className="txt-860">Login</span>
-          </button>
-        </div>
       </div>
       <Typography sx={{ mb: 2 }} variant="h4">
-        Inscrit toi!
+        Inscris toi!
       </Typography>
       <form
         className="contact-form"

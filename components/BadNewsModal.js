bb3b9c86a2007134734d9154734cc25f06059c56
JaavLex <alexandre.javet@hotmail.com>
2022-04-07 13:02:20 +0200

    [feat] Greatly improved bad news modal + Added social links
    - Social Networks links
    - App Logo
    - Small explanation of what happened

--- components/BadNewsModal.js
+++ components/BadNewsModal.js
@@ -1,25 +1,53 @@
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	TextField,
 } from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Box } from '@mui/system';
 
 export function BadNewsModal({ open, onClose }) {
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <DialogTitle>C'était fake!</DialogTitle>
-      <DialogContent>
-        <DialogContentText>C'était fake!!!</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button autoFocus onClick={onClose}>
-          Fermer
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog onClose={onClose} open={open}>
+			<DialogTitle>
+				<p className="txt-862">
+					Hang<span className="txt-8622">Now!</span>
+				</p>
+				Merci de ta participation !
+			</DialogTitle>
+			<DialogContent>
+				<DialogContentText>
+					Malheuresement, ce site est un prototype et n'est pas encore terminé, il ne
+					sert que de test pour l'instant. Nous espérons que tu trouveras ce concept
+					de site utile, n'hésite pas à nous faire part de tes remarques !
+					<Box
+						display="flex"
+						flexDirection="column"
+						justifyContent="center"
+						paddingTop={2}
+					>
+						<Box display="flex" gap={1}>
+							<MailIcon /> <span>apphangnow@gmail.com</span>
+						</Box>
+						<Box display="flex" gap={1}>
+							<TwitterIcon />
+							<span>
+								<a href="https://twitter.com/HangNowApp">@HangNowApp</a>
+							</span>
+						</Box>
+					</Box>
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button autoFocus onClick={onClose} variant="outlined">
+					Fermer
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
 }

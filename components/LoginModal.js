c9c0835dc5efd1cb42369377231602a428e0c666
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-04-07 13:15:07 +0200

    fix fix

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,61 +1,57 @@
 import {
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogContentText,
-	DialogTitle,
-	TextField,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
 } from '@mui/material';
 import mixpanel from 'mixpanel-browser';
 
 export function LoginModal({ open, onClose, onSuccess }) {
-	return (
-		<Dialog onClose={onClose} open={open}>
-			<DialogTitle>
-				<p className="txt-862">
-					Hang<span className="txt-8622">Now!</span>
-				</p>
-				<b>Inscris toi!</b>
-			</DialogTitle>
-			<form
-				// set no redirection to form
-				action="#"
-				method="POST"
-				onSubmit={e => {
-					e.preventDefault();
+  return (
+    <Dialog onClose={onClose} open={open}>
+      <DialogTitle>
+        <p className="txt-862">
+          Hang<span className="txt-8622">Now!</span>
+        </p>
+        <b>Inscris toi!</b>
+      </DialogTitle>
+      <form name="email" action="/success" method="POST" data-netlify="true" netlify>
+        <input type="hidden" name="form-name" value="email" />
 
-					onSuccess();
-
-					return false;
-				}}
-				name="email-login"
-				netlify
-			>
-				<DialogContent>
-					<DialogContentText>
-						Utilise ton email pour te connecter et pouvoir entrer en communication
-						avec la personne qui organise l'évènement.
-					</DialogContentText>
-					<TextField
-						autoFocus
-						margin="dense"
-						id="name"
-						label="Adresse Mail"
-						type="email"
-						fullWidth
-						variant="outlined"
-					/>
-				</DialogContent>
-				<DialogActions>
-					<Button autoFocus onClick={onClose} variant="outlined">
-						Fermer
-					</Button>
-					<Button type="submit" autoFocus onClick={onClose} variant="outlined">
-						Envoyer
-					</Button>
-				</DialogActions>
-			</form>
-		</Dialog>
-	);
+        <DialogContent>
+          <DialogContentText>
+            Utilise ton email pour te connecter et pouvoir entrer en communication
+            avec la personne qui organise l'évènement.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email"
+            type="email"
+            fullWidth
+            variant="standard"
+            name="email"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={onClose}>
+            Fermer
+          </Button>
+          <Button
+            onClick={() => {
+              mixpanel.track('Sign up');
+            }}
+            type="submit"
+            autoFocus
+          >
+            Envoyer
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
 }

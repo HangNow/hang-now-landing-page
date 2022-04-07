31aab859a141a0973502bca38184c11c3b11589b
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-04-07 13:31:17 +0200

    fuck ofF

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+
 export default function Success() {
-  return <div>Form successfully submitted!</div>;
+  return (
+    <div className="container">
+      <Head>
+        <title>My Portfolio | Success!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Form successfully submitted!</main>
+    </div>
+  );
 }
